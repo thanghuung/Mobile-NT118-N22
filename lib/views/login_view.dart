754a1b5a0116fa05f0fa9415e72b675ef0de28b3
ThangHuu <windnight57@gmail.com>
2023-03-21 17:30:14 +0700

    create app

---
+++ lib/views/login_view.dart
@@ -0,0 +1,20 @@
+import 'package:app/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+  
+}
